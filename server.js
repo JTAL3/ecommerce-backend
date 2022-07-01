8b8d52cc2c94df7a30fd06d7478b26eaad6ec57a
Jason Talbert <jason.talbert.jt@gmail.com>
2022-07-01 02:17:19 -0400

    trying to work connectivity bug

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+//getting connection error.
+//unable to find resolve but will revisit
+
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,5 +16,5 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+  app.listen(PORT, () => console.log(`App is currently on port ${PORT}`));
 });
